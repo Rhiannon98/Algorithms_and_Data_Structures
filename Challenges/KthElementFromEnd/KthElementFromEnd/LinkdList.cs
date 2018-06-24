8b0126fa040da3ca6bbb1df984971e8db86505ed
mortensenrhi <mortensenrhi@gmail.com>
2018-06-23 21:40:59 -0700

    KthElem sln

---
+++ Challenges/KthElementFromEnd/KthElementFromEnd/LinkdList.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KthElementFromEnd
+{
+    public class LinkdList
+    {
+        /// <summary>
+        /// Always points to the first node in a LL.
+        /// </summary>
+        public Node Head { get; set; }
+
+        /// <summary>
+        /// The node that is used to traverse through the LinkedList
+        /// We can add another node and call it something else
+        /// to traverse with the current if need be
+        /// </summary>
+        public Node Current { get; set; }
+
+        /// <summary>
+        /// Requiring at least one node to be present for a LL to be created
+        /// </summary>
+        /// <param name="node"></param>
+        public LinkdList(Node node)
+        {
+            Head = node;
+            Current = node;
+        }
+
+        //Adding a Node O(1)
+
+        public void Add(Node node)
+        {
+            node.Next = Head;
+            Head = node;
+            Current = Head;
+        }
+
+        // Print out Nodes O(n)
+
+        public void Print()
+        {
+            // setting current to equal head in most of these problems to 
+            Current = Head;
+
+            while (Current.Next != null)
+            {
+                Console.Write($"{Current.Value} --> ");
+                Current = Current.Next;
+            }
+
+            Console.Write($"{Current.Value} --> NULL");
+        }
+
+        /// <summary>
+        /// Finds a node with a specified Value. 
+        /// Big O of this is O(n)
+        /// </summary>
+        /// <param name="value">value to search</param>
+        /// <returns>Node that contains the value</returns>
+        public Node Find(int value)
+        {
+            Current = Head;
+
+            while (Current.Next != null)
+            {
+                if (Current.Value == value)
+                {
+                    return Current;
+                }
+                Current = Current.Next;
+            }
+
+            return Current.Value == value ? Current : null;
+        }
+
+        // Add Before O(n)
+
+        public void AddBefore(Node newNode, Node existingNode)
+        {
+            //Reset our Current to the beginning of the Linked List
+            // if we are not already there
+            Current = Head;
+            if (Head.Value == existingNode.Value)
+            {
+                Add(newNode);
+                return;
+            }
+
+            while (Current.Next != null)
+            {
+                if (Current.Next.Value == existingNode.Value)
+                {
+                    newNode.Next = existingNode;
+                    Current.Next = newNode;
+                    return;
+                }
+                Current = Current.Next;
+            }
+        }
+
+        // Add After O(n)
+        public void AddAfter(Node newNode, Node existingNode)
+        {
+            Current = Head;
+          
+            while (Current.Next != null)
+            {
+                if (Current.Value == existingNode.Value)
+                {
+                    newNode.Next = Current.Next;
+                    Current.Next = newNode;
+                    return;
+                }
+
+                Current = Current.Next;
+            }
+
+        }
+
+
+        // Add Last O(n)
+        // this function will only add the new node 
+        // but only after the end has been found
+        public void AddLast(Node newNode)
+        {
+            Current = Head;
+            while (Current.Next != null)
+            {
+                Current = Current.Next;
+            }
+
+            Current.Next = newNode;
+        }
+    }
+}
--- Challenges/KthElementFromEnd/KthElementFromEnd/Program.cs
+++ Challenges/KthElementFromEnd/KthElementFromEnd/Program.cs
@@ -2,11 +2,50 @@
 
 namespace KthElementFromEnd
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Kth Element from End! Whoop!");
+            Console.WriteLine("Now, pick a number and see if this works!");
+            int k = int.Parse(Console.ReadLine());
+            int theK = KthElement(k).Value;
+            Console.WriteLine(theK);
+            Console.ReadKey();
+        }
+        public static Node KthElement(int k)
+        {
+            LinkdList LL = new LinkdList(new Node(2));
+            LL.Add(new Node (3));
+            LL.Add(new Node (5));
+            LL.Add(new Node (7));
+            LL.Add(new Node (11));
+            LL.Add(new Node (13));
+            LL.Add(new Node (17));
+            LL.Add(new Node (19));
+            LL.Add(new Node (23));
+            LL.Add(new Node (29));
+            LL.Add(new Node (31));
+            LL.Add(new Node (37));
+            LL.Add(new Node (41));
+            LL.Add(new Node (43));
+            LL.Add(new Node (47));
+
+            LL.Print();
+
+            LL.Current = LL.Head;
+            Node Runner = LL.Head;
+            int counter = 0;
+            while (Runner.Next != null)
+            {
+                counter++;
+                Runner = Runner.Next;
+                if (counter > k)
+                {
+                    LL.Current = LL.Current.Next;
+                }
+            }
+            return LL.Current;
         }
     }
 }
